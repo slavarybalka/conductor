5b126571958b2153eb7dd408e02669c4e58cf0f5
Slava.Rybalka <slavarybalko@gmail.com>
2017-03-14 09:56:17 -0500

    Added some comments to the code

--- conductor.py
+++ conductor.py
@@ -29,6 +29,7 @@ conductor_signature = compute_signature(conductor_api_key, conductor_secret)
 
 combined_data = {}
 export_data = []
+export_data_dict = {}
 
 # substitute account_id and property_id with the ones you need
 r = requests.get("https://api.conductor.com/v3/12808/web-properties/59458/rank-sources/1/tp/CURRENT/serp-items?apiKey=" + conductor_api_key + "&sig=" + conductor_signature)
@@ -36,7 +37,7 @@ r = requests.get("https://api.conductor.com/v3/12808/web-properties/59458/rank-s
 # retrieving keywords in striking distance
 for elem2 in r.json():
 	#print elem2
-	if ("/programs/" in elem2['targetUrl'] or elem2['targetUrl'] == 'http://academicpartnerships.uta.edu') and elem2['ranks']['TRUE_RANK'] < 20:
+	if ("/programs/" in elem2['targetUrl'] or elem2['targetUrl'] == 'http://academicpartnerships.uta.edu') and elem2['ranks']['TRUE_RANK'] < 20 and elem2['targetUrl'] != "http://academicpartnerships.uta.edu/programs/":
 		if elem2['ranks']['TRUE_RANK'] > 10:
 			combined_data[str(elem2['trackedSearchId'])] = [elem2['ranks']['TRUE_RANK'], elem2['targetUrl']]
 
@@ -49,31 +50,38 @@ for elem1 in t.json():
 		if elem1['trackedSearchId'] == key:
 			combined_data[key].append(elem1['queryPhrase'])
 
-#print combined_data
+for key, value in combined_data.iteritems():
+	print key, value
 
 # SEMRush: "This report provides an extended list of related keywords, synonyms, and variations relevant to a queried term in a chosen database."
 
 for key, value in combined_data.iteritems():
 	# setting a delay between SEMRush requests
 	time.sleep(3)										
-	
+	# getting related searches for each keyword
 	s = requests.get('http://api.semrush.com/?type=phrase_related&key=' + semrush_api_key + '&display_limit=10&export_columns=Ph,Nq&phrase=' + value[2] + '&database=us')
-	result = s.text.split('\r') 						# getting related searches for each keyword
-	if result == [u'ERROR 50 :: NOTHING FOUND']:		# if nothing is found then just skip it
+	result = s.text.split('\r') 						
+	# if nothing is found then just skip it
+	if result == [u'ERROR 50 :: NOTHING FOUND']:		
 		pass
+	# if related keywords are found, add them to the dictionary	
 	else:
+		# print the source keyword
 		print "\nSource keyword:", value[2]
 		print "\nRelated keywords:"
 		
-		#retrieve the contents of the URL
-		pr = requests.get(value[1])
+		#if any related keyword is found then retrieve the contents of the related URL
 
-		for item in result[1:]:							# if some related keywords are returned then 
+		pr = requests.get(value[1])
+		# iterate over the list of returned related keywords
+		for item in result[1:]:	
+		# if some related keywords are returned then 						
 			semrush_list = item.strip().split(';')
 
 			# checking if the keyword exists on page
 			if semrush_list[0] not in pr.text:
 			#if it doesn't exist, append it to the list
+				#export_data_dict = 
 				export_data.append([value[2], semrush_list[0], semrush_list[1], value[1]])
 
 
